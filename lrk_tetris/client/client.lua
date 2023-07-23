ac9dc48954a88859e51c1e6b0afb0966470eaf2d
lucekrk <100790348+lucekrk@users.noreply.github.com>
2023-07-23 02:11:29 +0200

    Update client.lua

--- lrk_tetris/client/client.lua
+++ lrk_tetris/client/client.lua
@@ -1,12 +1,3 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(1000)
-    end
-end)
-
 local MinigameActive = false
 local MinigameFinished = false
 local Success = false
@@ -42,7 +33,6 @@ end)
 RegisterNUICallback('udane', function(data, cb)
 SetNuiFocus(false, false)
 Success = true
-ESX.ShowNotification("udaj sie tam i tu")
 MinigameFinished = false
 MinigameActive = false
 cb('ok')
@@ -54,28 +44,3 @@ MinigameActive = false
 Success = false
 cb('ok')
 end)
-
-RegisterNetEvent('startOko', function()
-    SetNuiFocus(true, true)
-    SendNUIMessage({type = 'otworz'})
-end)
----
-exports.qtarget:AddBoxZone("startTetrisZone", vector3(-292.15, -957.85, 31.21), 2, 1, {
-	name="startTetrisZone",
-	heading=11.0,
-	debugPoly=false,
-	minZ=28.77834,
-	maxZ=32.87834,
-	}, {
-		options = {
-			{
-				event = "startOko",
-				icon = "fa-solid fa-virus",
-				label = "hackuj",
-			},
-		},
-		distance = 3.5
-})
-
-
----vector3(-292.15, -957.85, 31.21), 2, 1, 
